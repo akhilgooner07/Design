e82e10cfa164eb0bd6c6d10c017feb0bd0d8acc0
Anmol Gautam <tarptaeya@gmail.com>
2017-10-19 16:24:23 +0530

    added basic functions

--- server/server.js
+++ server/server.js
@@ -1,22 +1,75 @@
 const app = require('express')()
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(cookieParser)
 
-app.listen(9017)
+app.listen(9000)
+
+app.get('/', function(req,res){
+    console.log(req)
+    res.send('Hello World')
+})
+
+userData = {}
+
+/*
+
+userData = {
+    
+    {
+        "id": "...",
+        "groupid": "...",
+        "currentClaim": "...",
+        "totalReports": "...",
+        "totalClaims": "...",
+    }
+
+}
+
+*/
+
+groupData = {}
+
+/*
+
+{
+    {
+        "id": "...",
+        "userids": [
+            ...
+        ]
+    }
+}
+
+*/
 
 app.post('/claim', function(req,res){
-
+    var userid = res.cookie('design', 'userID')
+    for(user in userData){
+        if(user.id == userid){
+            user.currentClaim = true
+            user.totalClaims ++
+        }
+    }
 })
 
 app.post('/reportclaim/:usrid', function(req,res){
-
+    var groupid = res.cookie('design', 'groupID')
+    var userid = req.params.userid
+    for(user in userData){
+        if(user.id == userid && user.groupid == groupid){
+            user.totalReports ++
+        }
+    }
+    // TODO : set a cookie for 1 day
 })
 
 app.get('/grouplist', function(req,res){
-
+    res.writeHead(200, {'content-type': 'text/json'})
+    res.end(groupData)
 })
 
 app.post('/joingroup/:grpid', function(req,res){
@@ -27,14 +80,12 @@ app.post('/exitgroup/:grpid', function(req,res){
 
 })
 
-app.post('/truth/:usrid', function(req,res){
-
-})
-
-app.get('userlist/:grpid', function(req,res){
-
-})
-
 app.get('reputation/:usrid', function(req,res){
-
+    var userid = req.params.userid
+    for(user in userData){
+        if(user.id == userid){
+            res.send(user.totalClaims - user.totalReports)
+            break
+        }
+    }
 })

565dff01ccab10dca5bc88f33bcb2ce759488078
Anmol Gautam <tarptaeya@gmail.com>
2017-10-25 01:18:46 +0530

    implemented ui for login screen && user and group class

--- server/server.js
+++ server/server.js
@@ -1,50 +1,73 @@
-const app = require('express')()
+const express = require('express')
+const app = express()
 const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
+
+app.set('view engine', 'pug')
+app.set('views', './views')
+
+app.use(express.static('./img'))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
-app.use(cookieParser)
 
-app.listen(9000)
+app.get('/login', function(req,res){
+  res.render('authenticate', {'login':true})
+})
 
-app.get('/', function(req,res){
-    console.log(req)
-    res.send('Hello World')
+app.get('/signup', function(req,res){
+  res.render('authenticate', {'login':false})
 })
 
 userData = {}
 
-/*
-
-userData = {
-    
-    {
-        "id": "...",
-        "groupid": "...",
-        "currentClaim": "...",
-        "totalReports": "...",
-        "totalClaims": "...",
-    }
-
+function User(name, password, roll_no){
+  this.name = name
+	this.password = password
+	this.id = roll_no
+	this.groupid = null
+	function setGroupId(group_id){
+	  this.groupid = group_id
+	}
+	this.claims = []
+	this.totalClaims = 0
+	this.totalReports = 0
+	this.doClaim = function(){
+	  this.totalClaims ++
+		this.claim.push(new Date(Date.now()))
+		return true
+	}
+	this.doReport = function(userId){
+	  for(user in userData){
+		  if(user.id == userId){
+			  user.totalReports ++
+				break
+			}
+		}
+		return true
+	}
+	this.getReputation = function(){
+	  return this.totalClaims - this.totalReports
+	}
+	this.joinGroup = function(groupId){
+	  for(group in groupData){
+		  if(group.id == groupId){
+			  this.groupid = group.id
+				group.users.push(this)
+				break
+			}
+		}
+	}
 }
-
-*/
 
 groupData = {}
 
-/*
 
-{
-    {
-        "id": "...",
-        "userids": [
-            ...
-        ]
-    }
+function Group(name, id){
+  this.name = name
+	this.id = id
+	this.users = []
 }
 
-*/
 
 app.post('/claim', function(req,res){
     var userid = res.cookie('design', 'userID')
@@ -89,3 +112,5 @@ app.get('reputation/:usrid', function(req,res){
         }
     }
 })
+
+app.listen(9000)
